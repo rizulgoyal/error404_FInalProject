dc999507e7032ced6668fbc549a43a738d179386
rizul <rizulgoyal23@gmail.com>
2020-01-23 17:31:59 -0500

    ui for add category

---
+++ error404_FInalProject/View Controllers/AddCategoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddCategoryViewController.swift
+//  error404_FInalProject
+//
+//  Created by Rizul goyal on 2020-01-23.
+//  Copyright © 2020 Rizul goyal. All rights reserved.
+//
+
+import UIKit
+
+class AddCategoryViewController: UIViewController {
+
+    @IBOutlet var labelCategory: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
