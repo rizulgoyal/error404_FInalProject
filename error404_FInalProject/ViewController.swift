f896c6eb1c38716c701503e8249d803cc1cab578
rizul <rizulgoyal23@gmail.com>
2020-01-22 20:41:35 -0500

    Initial Commit

---
+++ error404_FInalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  error404_FInalProject
+//
+//  Created by Rizul goyal on 2020-01-22.
+//  Copyright © 2020 Rizul goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
